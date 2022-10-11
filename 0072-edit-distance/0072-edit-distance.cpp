ef0dd3da908b9e12c87c230d03fc32f8cab04ba0
Amruta-Pawar2001 <87181408+Amruta-Pawar2001@users.noreply.github.com>
2022-10-11 14:21:48 +0530

    Time: 17 ms (70.71%), Space: 9.1 MB (40.63%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,19 +1,41 @@
 class Solution {
 public:
-    int fun(string word1, string word2 , int i , int j, vector<vector<int>> &dp)
+    int fun(string word1, string word2)
     {
-        if(i<0)
-            return j+1;
-        if(j<0)
-            return i+1;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        if(word1[i]==word2[j])
-            return fun(word1, word2, i-1,j-1,dp);
-        return dp[i][j] = 1 + min(fun(word1 , word2 , i-1 , j,dp), min(fun(word1 , word2 , i-1,j-1,dp),fun(word1, word2, i , j-1,dp)));
+        // if(i<0)
+        //     return j+1;
+        // if(j<0)
+        //     return i+1;
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i =0;i<=n;i++)
+        {
+            dp[i][0] = i;
+        }
+        for(int j=0;j<=m;j++)
+        {
+            dp[0][j] = j;
+        }
+        // if(dp[i][j]!=-1)
+        //     return dp[i][j];
+        // if(word1[i]==word2[j])
+            
+            // return fun(word1, word2, i-1,j-1,dp);
+        // return dp[i][j] = 1 + min(fun(word1 , word2 , i-1 , j,dp), min(fun(word1 , word2 , i-1,j-1,dp),fun(word1, word2, i , j-1,dp)));
+        for(int i =1;i<n+1;i++)
+        {
+            for(int j=1;j<m+1;j++)
+            {
+                if(word1[i-1]==word2[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = 1 + min(dp[i-1][j],min(dp[i-1][j-1],dp[i][j-1]));
+            }
+        }
+        return dp[n][m];
     }
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size(),vector<int>(word2.size(),-1));
-        return fun(word1 , word2 , word1.size()-1 , word2.size()-1,dp);
+        return fun(word1 , word2 );
     }
 };
