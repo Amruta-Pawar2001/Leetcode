c3db574c2f776f7008f9f6da9ffcbfa8318a9a36
Amruta-Pawar2001 <87181408+Amruta-Pawar2001@users.noreply.github.com>
2022-12-13 06:13:49 +0530

    Time: 100 ms (99.38%), Space: 61.2 MB (92.21%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -15,21 +15,36 @@ public:
   // return slow;
        
        
-       int ans ;
-       unordered_map<int,int> freq;
-       for(auto x: nums)
+       // int ans ;
+       // unordered_map<int,int> freq;
+       // for(auto x: nums)
+       // {
+       //     freq[x]++;
+       // }
+       // int n = nums.size();
+       // for(int i=0;i<n;i++)
+       // {
+       //     if(freq[nums[i]]>1)
+       //     {
+       //         ans = nums[i];
+       //         break;
+       //     }
+       // }
+       // return ans;
+       
+       
+       int slow = nums[0];
+       int fast = nums[0];
+       do{
+           slow = nums[slow];
+           fast = nums[nums[fast]];
+       }while(slow!=fast);
+       fast = nums[0];
+       while(slow!=fast)
        {
-           freq[x]++;
+           slow = nums[slow];
+           fast = nums[fast];
        }
-       int n = nums.size();
-       for(int i=0;i<n;i++)
-       {
-           if(freq[nums[i]]>1)
-           {
-               ans = nums[i];
-               break;
-           }
-       }
-       return ans;
+       return slow;
 }
 };
