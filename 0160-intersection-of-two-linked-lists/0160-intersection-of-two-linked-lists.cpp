07e75082aba240019528172615f53f5d7f0b9d8d
Amruta-Pawar2001 <87181408+Amruta-Pawar2001@users.noreply.github.com>
2022-12-19 23:21:43 +0530

    Time: 52 ms (86.54%), Space: 14.6 MB (74.76%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -0,0 +1,82 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA==NULL||headB==NULL)return NULL;
+        ListNode *d1 = headA;
+        ListNode *d2 = headB;
+        while(d1!=d2)
+        {
+            d1 = d1==NULL?headB:d1->next;
+            d2 = d2==NULL?headA:d2->next;
+        }
+        return d1;
+    }
+        // ListNode *d1 = headA;
+        // ListNode *d2 = headB;
+        // if(headA==NULL||headB==NULL)
+        //     return NULL;
+        // while(d1!=d2)
+        // {
+        //     d1=d1->next;
+        //     d2=d2->next;
+        //     if(d1==NULL)
+        //     {
+        //         d1=headB;
+        //     }
+        //     else
+        //         d2=headA;
+        // }
+        // return d1;
+    // }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     ListNode *d1=headA;
+//     ListNode *d2=headB;
+//         if(headA==NULL||headB==NULL)
+//             return NULL;
+//     while(d1!=d2)
+//     {
+//         d1=d1->next;
+//         d2=d2->next;
+//         if(d1==NULL)
+//         {
+//             d1=headB;
+//         }
+//         else
+//         if(d2==NULL)
+//         {
+//             d2=headA;
+//         }
+//     }
+//     return d1;
+// }
+};
