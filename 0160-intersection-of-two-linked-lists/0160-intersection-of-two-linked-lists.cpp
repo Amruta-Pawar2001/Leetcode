fa034d8b2a82df8216c49ab483366e19f6d251a7
Amruta-Pawar2001 <87181408+Amruta-Pawar2001@users.noreply.github.com>
2022-12-19 23:24:57 +0530

    Time: 44 ms (96.95%), Space: 14.6 MB (34.51%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,16 +9,16 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if(headA==NULL||headB==NULL)return NULL;
-        ListNode *d1 = headA;
-        ListNode *d2 = headB;
-        while(d1!=d2)
-        {
-            d1 = d1==NULL?headB:d1->next;
-            d2 = d2==NULL?headA:d2->next;
-        }
-        return d1;
-    }
+    //     if(headA==NULL||headB==NULL)return NULL;
+    //     ListNode *d1 = headA;
+    //     ListNode *d2 = headB;
+    //     while(d1!=d2)
+    //     {
+    //         d1 = d1==NULL?headB:d1->next;
+    //         d2 = d2==NULL?headA:d2->next;
+    //     }
+    //     return d1;
+    // }
         // ListNode *d1 = headA;
         // ListNode *d2 = headB;
         // if(headA==NULL||headB==NULL)
@@ -59,24 +59,17 @@ public:
     
     
     
-//     ListNode *d1=headA;
-//     ListNode *d2=headB;
-//         if(headA==NULL||headB==NULL)
-//             return NULL;
-//     while(d1!=d2)
-//     {
-//         d1=d1->next;
-//         d2=d2->next;
-//         if(d1==NULL)
-//         {
-//             d1=headB;
-//         }
-//         else
-//         if(d2==NULL)
-//         {
-//             d2=headA;
-//         }
-//     }
-//     return d1;
-// }
+    ListNode *d1=headA;
+    ListNode *d2=headB;
+        if(headA==NULL||headB==NULL)
+            return NULL;
+    while(d1!=d2)
+    {
+        if(d1==NULL) d1=headB;
+        else d1=d1->next;
+        if(d2==NULL) d2=headA;
+        else d2=d2->next;
+    }
+    return d1;
+}
 };
