ff2cf0e33f5a600872836c5d0f6438f973101fed
Amruta-Pawar2001 <87181408+Amruta-Pawar2001@users.noreply.github.com>
2022-12-19 23:02:38 +0530

    Time: 50 ms (72.80%), Space: 71.4 MB (52.14%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,91 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {      
+        int carry =0 ;
+        int sum , res;
+        ListNode *dummy = new ListNode();
+        ListNode *temp = dummy;
+        while(l1!=NULL || l2!=NULL || carry)
+        {         
+            sum =0;
+            if(l1!=NULL)
+            {
+                sum += l1->val;
+                l1=l1->next;
+            }
+            if(l2!=NULL)
+            {
+                sum += l2->val;
+                l2=l2->next;
+            }
+            
+            sum += carry;
+            carry = sum/10;  
+            ListNode *node = new ListNode(sum%10);
+            temp->next =node;
+            temp=temp->next;
+        }
+        
+       return dummy->next; 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         int carry = 0;
+//         int sum = 0;
+//         Listnode *dummy = NULL;
+//         Listnode *temp = dummy;
+//         while(l1!=NULL || l2!=NULL || carry)
+//         {
+//             sum =0;
+//             if(l1!=NULL)
+//             {
+//                 sum+=l1->value;
+//                 l1=l1->next;
+//             }
+//             if(l2!=NULL)
+//             {
+//                 sum+=l2->value;
+//                 l2=l2->next;
+//             }
+//             sum+=carry;
+//             carry = sum/10;
+//             Listnode *node = new Listnode(sum%10);
+//             temp->next = node;
+//             temp= temp->next;
+            
+//         }
+//         return dummy->next;
+        
+}
+        
+};
